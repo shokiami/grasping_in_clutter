16948934e9f56c5aaf2d4f4cfec7ef20040e0eda
shokiami <kiami.sho@gmail.com>
2023-04-25 19:28:46 -0700

    cleaned up urdfs

--- env.py
+++ env.py
@@ -1,28 +1,20 @@
 import pybullet as p
-import numpy as np
 import time
 import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+
+physicsClient = p.connect(p.GUI)
 p.setGravity(0,0,-10)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-# handid = p.loadURDF("ability-hand-api/URDF/ability_hand_right.urdf", [0,0,0.5], [0,0,0,1], useFixedBase=True, globalScaling=1)
-#arm_hand = p.loadURDF("xarm/xarm5_robot_psyonic.urdf", [0,0,0.3], [0,0,0,1], useFixedBase=True, globalScaling=1)
-arm_hand = p.loadURDF("franka/deps/Panda/panda.urdf", [0,0,0.3], [0,0,0,1], useFixedBase=True, globalScaling=1)
-# p.changeDynamics(handid, -1, mass=10)
-
-# control the hand
-print(p.getNumJoints(arm_hand))
+arm_hand = p.loadURDF("urdfs/franka/panda.urdf", startPos, startOrientation, useFixedBase=True)
 
 for i in range (10000):
-    # p.setJointMotorControl2(handid, )
-    random_rot = 5#np.random.uniform(-1.57, 1.57)
-    p.setJointMotorControl2(arm_hand, 2, p.POSITION_CONTROL, targetPosition=random_rot, force =100)
+    random_rot = 5
+    p.setJointMotorControl2(arm_hand, 2, p.POSITION_CONTROL, targetPosition=random_rot, force=100)
     p.stepSimulation()
     time.sleep(1./24.)
-# cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-# print(cubePos,cubeOrn)
+
 p.disconnect()
