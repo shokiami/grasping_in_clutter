07a83f2463e8a9952041eb2c107445b94685e5f3
shokiami <kiami.sho@gmail.com>
2023-04-27 18:06:10 -0700

    added confusion matrix

--- src/classifier.py
+++ src/classifier.py
@@ -1,25 +1,23 @@
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
 
-OBJECTS = ['duck', 'marker', 'ball', 'cup', 'horse']
+OBJECTS = ['marker', 'duck', 'ball', 'cup', 'horse']
 
 if __name__ == '__main__':
   X = []
   y = []
-  for i in range(len(OBJECTS)):
-    object = OBJECTS[i]
+  for object in OBJECTS:
     with open(f'../data/{object}_pos.csv', 'r') as pos_csv, open(f'../data/{object}_touch.csv', 'r') as touch_csv:
       for pos_row, touch_row in zip(pos_csv, touch_csv):
-        X.append(eval(pos_row) + eval(touch_row))
-        y.append(i)
+        X.append(eval(touch_row))
+        y.append(object)
 
   X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-  scaler = MinMaxScaler()
-  X_train = scaler.fit_transform(X_train)
-  X_test = scaler.transform(X_test)
 
-  knn = KNeighborsClassifier()
-  knn.fit(X_train, y_train)
-  print(f'train accuracy: {knn.score(X_train, y_train)}')
-  print(f'test accuracy: {knn.score(X_test, y_test)}')
+  model = DecisionTreeClassifier()
+  model.fit(X_train, y_train)
+  print(f'train accuracy: {model.score(X_train, y_train)}')
+  print(f'test accuracy: {model.score(X_test, y_test)}')
+  print('confusion matrix:')
+  print(confusion_matrix(y_test, model.predict(X_test)))
