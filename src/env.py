b67329582553c28e4aea23a066648695ced87b45
shokiami <kiami.sho@gmail.com>
2023-05-08 17:27:23 -0700

    inverse kinematics working

--- src/env.py
+++ src/env.py
@@ -1,62 +1,78 @@
 import pybullet as p
-import numpy as np
 import time
 import pybullet_data
 import gym
 
-NUM_JOINTS = 25
-
 class FrankaPsyonic(gym.Env):
   def __init__(self):
+    # load env
     self.p = p
     self.p.connect(self.p.GUI)
-    self.p.setGravity(0,0,-10)
+    self.p.setGravity(0, 0, -9.81)
     self.p.setAdditionalSearchPath(pybullet_data.getDataPath())
     self.p.loadURDF('plane.urdf')
-    initPosition = [0, 0, 0]
-    initOrientation = [0, 0, 0]
-    self.franka_psyonic_id = self.p.loadURDF('../urdfs/franka/panda.urdf', initPosition, self.p.getQuaternionFromEuler(initOrientation), useFixedBase=True)
-    self.createObject('duck', [0.5, 0, 0], [90, 0, 90], 1.0)
 
-  def createObject(self, name, initPosition, initOrientation, mass):
+    # load robot
+    self.dof = 7
+    self.kp = 0.01
+    self.kd = 1.0
+    self.max_torque = 100
+    self.robot = self.p.loadURDF('../urdfs/franka/panda_official.urdf', [0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0], useFixedBase=True)
+    self.num_joints = self.p.getNumJoints(self.robot)
+    print(f'num_joints: {self.num_joints}')
+
+    # load duck
+    self.duck = self.createObject('duck', [0.5, 0, 0], [90, 0, 90], 1.0)
+
+    # reset robot
+    self.reset()
+
+  def createObject(self, name, initPosition, init_ori, mass):
     meshScale = [0.1, 0.1, 0.1]
     visualShapeId = self.p.createVisualShape(shapeType=self.p.GEOM_MESH,
-                                        fileName=f'{name}.obj',
-                                        rgbaColor=[1, 1, 1, 1],
-                                        specularColor=[0.4, .4, 0],
-                                        meshScale=meshScale)
+                                            fileName=f'{name}.obj',
+                                            rgbaColor=[1, 1, 1, 1],
+                                            specularColor=[0.4, .4, 0],
+                                            meshScale=meshScale)
     collisionShapeId = self.p.createCollisionShape(shapeType=self.p.GEOM_MESH,
-                                              fileName=f'{name}_vhacd.obj',
-                                              meshScale=meshScale)
-    self.p.createMultiBody(baseMass=mass,  # kg
-                          baseInertialFramePosition=[0, 0, 0],
-                          baseCollisionShapeIndex=collisionShapeId,
-                          baseVisualShapeIndex=visualShapeId,
-                          basePosition=initPosition,
-                          baseOrientation=self.p.getQuaternionFromEuler(initOrientation),
-                          useMaximalCoordinates=True)
-
+                                                  fileName=f'{name}_vhacd.obj',
+                                                  meshScale=meshScale)
+    return self.p.createMultiBody(baseMass=mass, # kg
+                                  baseInertialFramePosition=[0, 0, 0],
+                                  baseCollisionShapeIndex=collisionShapeId,
+                                  baseVisualShapeIndex=visualShapeId,
+                                  basePosition=initPosition,
+                                  baseOrientation=init_ori,
+                                  useMaximalCoordinates=True)
 
   def reset(self):
-    default_joints = np.zeros(NUM_JOINTS)
-    for i in range(NUM_JOINTS):
-      self.p.resetJointState(self.franka_psyonic_id, i, default_joints[i])
+    for i in range(self.dof):
+      joint_info = self.p.getJointInfo(self.robot, i)
+      min_pos = joint_info[8]
+      max_pos = joint_info[9]
+      self.p.resetJointState(self.robot, i, 0.5 * min_pos + 0.5 * max_pos)
 
   def step(self, action):
-    target_position = action
-    target_orientation = self.p.getQuaternionFromEuler([0, 0, 0])
-    end_effector = 6
-    joint_angles = self.p.calculateInverseKinematics(self.franka_psyonic_id, end_effector, target_position, target_orientation)
-    for i in range(NUM_JOINTS):
-      target_joint_angle = joint_angles[i] if i <= end_effector else 0
-      self.p.setJointMotorControl2(self.franka_psyonic_id, i, self.p.POSITION_CONTROL, targetPosition=target_joint_angle, force=1000)
+    target_pos, target_ori = action
+    joint_angles = list(self.p.calculateInverseKinematics(self.robot, self.dof, target_pos, target_ori))
+    for i in range(self.dof):
+      self.p.setJointMotorControl2(bodyUniqueId=self.robot,
+                                  jointIndex=i,
+                                  controlMode=self.p.POSITION_CONTROL,
+                                  targetPosition=joint_angles[i],
+                                  force=self.max_torque,
+                                  positionGain=self.kp,
+                                  velocityGain=self.kd)
     self.p.stepSimulation()
 
 if __name__ == '__main__':
   env = FrankaPsyonic()
   env.reset()
+  start_t = time.time()
   while True:
-    t = time.time()
-    action = [0.4 * np.cos(t), 0.4 * np.sin(t), 0.8]
+    target_pos = list(env.p.getBasePositionAndOrientation(env.duck)[0])
+    target_pos[2] += 0.5
+    target_ori = [1.0, 0.0, 0.0, 0.0] # downward
+    action = (target_pos, target_ori)
     env.step(action)
     time.sleep(1./120.)
